d2efc59c0549e32cf3425f95de2bba373c1c7878
Barış Ertakuş <bariserta@gmail.com>
2022-04-14 04:03:58 +0300

    Add sortRoomMessages & getLastMessages

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const userRoutes = require("./routes/userRoutes");
-
+const User = require("./models/User");
+const Message = require("./models/Message");
 const rooms = ["general", "technology", "business"];
 const cors = require("cors");
 
@@ -9,9 +10,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-
-app.use("/users",userRoutes);
+app.use("/users", userRoutes);
 require("./connection");
+
 
 const server = require("http").createServer(app);
 const PORT = 4000;
@@ -21,6 +22,30 @@ const io = require("socket.io")(server, {
     methods: ["GET", "POST"],
   },
 });
+
+app.get("/rooms", (req, res) => {
+  res.json(rooms);
+});
+
+const getLastMessagesFromRoom = async (room) => {
+  let roomMessages = await Message.aggregate([
+    { $match: { to: room } },
+    { $group: { _id: "$date", messagesByDate: { $push: "$$ROOT" } } },
+  ]);
+  return roomMessages;
+};
+
+const sortRoomMessagesByDate = (messages) => {
+  return messages.sort((a, b) => {
+    let date1 = a._id.split("/");
+    let date2 = b._id.split("/");
+
+    date1 = date1[2] + date1[0] + date1[1];
+    date2 = date2[2] + date2[0] + date2[1];
+
+    return date1 < date2 ? -1 : 1;
+  });
+};
 
 server.listen(PORT, () => {
   console.log("Server is working. Port:", PORT)
